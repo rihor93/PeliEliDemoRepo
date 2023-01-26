0146cf68a7789fd3c3e3c08df745fa463acca76b
Александр <rihor93@gmail.com>
2023-01-26 20:56:17 +0500

    добавил обводку, заставил работать

---
+++ src/component/UserInfo/UserCard.tsx
@@ -0,0 +1,20 @@
+import React, { useCallback, useEffect, useState } from "react";
+
+interface UserCardProps {
+    name: string,
+    bonuses: string,
+}
+
+const UserCard: React.FC<UserCardProps> = (props) => {
+
+    return (
+
+        <div className="contentWrapper">
+            <h1>Добрый день, {props.name}!</h1>
+            <h1>Вам доступно, {props.bonuses} бонусных балов!</h1>
+        </div>
+
+    );
+}
+
+export default UserCard;
--- src/component/UserInfo/UserInfo.tsx
+++ src/component/UserInfo/UserInfo.tsx
@@ -1,13 +1,29 @@
 import { type } from "@testing-library/user-event/dist/type";
-import React, { useCallback, useEffect } from "react";
+import { Divider } from "antd";
+import React, { useCallback, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { updateExpressionStatement } from "typescript";
 import { useTelegram } from "../../Hook/useTelegram";
 import TelegramButton from "../TelegramButton/TelegramButton";
+import ActionCard from "./ActionCard";
+import UserCard from "./UserCard";
 
-
+interface Action {
+    Name: string,
+    Description: string,
+    VCode: number,
+}
 
 const UserInfo: React.FC = () => {
-    const { tg, queryId } = useTelegram();
+    const { tg, userID } = useTelegram();
+
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(false)
+    const [userName, setUserName] = useState('');
+    const [userBosuses, setUserBosuses] = useState('');
+    const [userAuthorized, setUserAuthorized] = useState(false);
+    const [userActions, setUserActions] = useState<Action[]>();
+
     const navigate = useNavigate()
     useEffect(() => {
         tg.ready();
@@ -21,17 +37,63 @@ const UserInfo: React.FC = () => {
         }
     })
 
+    useEffect(() => {
+        console.log('загрузка данных с сервера')
+        loadUserInfo();
+    }, [])
+
     const onClose = () => {
         tg.close();
     }
 
-    
+    const loadUserInfo = async () => {
+        try {
+            const response = await fetch('https://elipelisr.lexcloud.ru/elipelibot/getUserInfo/' + userID, {
+                method: 'get',
+            })
+            //console.log({ FAQData: data });
+            if (response.status === 200) {
+
+                let data = JSON.parse(await response.text()).recordsets;
+                console.log(data);
+                //const blob = await response.
+                if (data[0].length > 0) {
+                    setUserAuthorized(true);
+                    setUserName(data[0][0].NAME)
+                    setUserBosuses(data[0][0].Bonuses.toFixed(2))
+                }
+                setUserActions(data[1])
+                setLoading(false);
+            }
+        } catch (ex) {
+            console.error(ex);
+            setError(true);
+        }
+    }
 
     return (
 
         <div className="contentWrapper">
-            <TelegramButton value='FAQ' onClick={() => { navigate('/faq') }} />
-            <TelegramButton value='Получить консультацию по продукту' onClick={() => { navigate('/consultation') }} />
+            {
+                loading ?
+                    <div>Идёт загрузка, пожалуйста подождите</div> :
+                    (
+                        <div>
+                            {(userAuthorized ? <div>{<UserCard name={userName} bonuses={userBosuses} />}</div> : <div><h1>К сожалению, вы еще не завели карточку в нашей кулинарии</h1></div>)}
+                            <div>
+                                <div><h1>Акции, специально для вас</h1></div>
+                                <div>
+                                    {userActions?.map((act, i) => {
+                                        return <ActionCard name={act.Name} description={act.Description}></ActionCard>
+                                    })}
+                                </div>
+                            </div>
+                        </div>
+                    )
+
+            }
+            {/*<TelegramButton value='FAQ' onClick={() => { navigate('/faq') }} />
+            <TelegramButton value='Получить консультацию по продукту' onClick={() => { navigate('/consultation') }} />*/}
             {/*<TelegramButton value='Задать вопрос' onClick={botApi.onSendDataPresent} />*/}
         </div>
 
