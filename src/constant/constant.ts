a035de649566896773f25b9aea6e665526c3024f
Александр <rihor93@gmail.com>
2023-05-31 11:52:16 +0500

    поправил адрес сервера

--- src/constant/constant.ts
+++ src/constant/constant.ts
@@ -1,3 +1,3 @@
 export const useDebug = true;
-export const telegramBotUrl = 'http://localhost:8080/api/v2';
-//export const telegramBotUrl = 'https://elipelisr.lexcloud.ru/elipelibot';
+//export const telegramBotUrl = 'http://localhost:8080/api/v2';
+export const telegramBotUrl = 'https://elipelisr.lexcloud.ru/elipelibot';
