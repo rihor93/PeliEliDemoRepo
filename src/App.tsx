d1a513d103c1a7c8f6633ed7bdfcd8b075cdc860
lykasov_am <lykasov_am@lexema.ru>
2023-06-02 15:31:30 +0500

    CHORE: remove unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
 import React from 'react';
 import './App.css';
-import Header from './component/Header/Header';
-import { Provider } from 'react-redux';
-import store from './store';
-import { Navigation } from './component/Navigation/Navigation';
-
-
 
 function App() {
   return (
-    <div className="container">
-      <Provider store={store}>
-        <Header />
-        <div className="contentContainer">
-          <Navigation />
-        </div>
-      </Provider >
-    </div >
+    <div>app</div>
   );
 }
 
