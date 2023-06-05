2ebcf615cbc835f224403bbc744594bfebcce228
lykasov_am <lykasov_am@lexema.ru>
2023-06-05 13:27:16 +0500

    UPD: rootstore

--- src/store/RootStore.tsx
+++ src/store/RootStore.tsx
@@ -1,9 +1,38 @@
 import React from "react";
-import { makeAutoObservable } from "mobx";
+import { makeAutoObservable, reaction } from "mobx";
+import { AuthStore } from "./stores";
 
 export class Store {
+  auth: AuthStore = new AuthStore(this);
+  subscriptions: (() => void)[] = [];
+
   constructor() { 
-    makeAutoObservable(this) 
+    makeAutoObservable(this);
+    const dispose = reaction(
+      () => this.auth.isAuthorized,
+      (value, prevValue) => {
+        if (value === true && prevValue === false) {
+          this.afterAuthorized()
+        }
+      }
+    );
+    
+    this.subscriptions.push(dispose);
+  }
+  // Загружаются данные связаные с учеткой
+  afterAuthorized() {
+    console.log('after authorized')
+  }
+
+  // Загружаются общие данные, главные страницы и т.д.
+  afterLoaded() {
+    console.log('after loaded')
+  }
+
+  onDestroy() {
+    for (const unsubscribe of this.subscriptions) {
+      unsubscribe();
+    }
   }
 }
 
