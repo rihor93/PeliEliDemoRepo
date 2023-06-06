2fe3a37bf3b6a45b808fb3899f3f40ffd2757fc9
lykasov_am <lykasov_am@lexema.ru>
2023-06-06 11:38:17 +0500

    ADD: http axios client class

---
+++ src/common/features/http-client.ts
@@ -0,0 +1,92 @@
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
+import { config } from "../configuration";
+
+export type QueryStringParams = string | Record<string, any> | [string, any][];
+
+export interface RequestError {
+  message: string;
+}
+
+export const REFRESH_TOKEN = "REFRESH";
+
+class HttpClient {
+  // static instance: HttpClient;
+  // static inject = () => HttpClient.instance;
+  private client: AxiosInstance;
+
+  constructor() {
+    // HttpClient.instance = this
+    this.client = axios.create({
+      baseURL: config.apiURL,
+    });
+  }
+
+  async get<T>(
+    url: string,
+    parameters: QueryStringParams = "",
+    config: Omit<AxiosRequestConfig, "method" | "url"> = {}
+  ): Promise<T> {
+    const searchParams = new URLSearchParams(parameters);
+    const searchParamsString = searchParams.toString();
+    return this.client
+      .get<T | RequestError>(
+        url + (searchParamsString.length > 0 ? `?${searchParamsString}` : ""),
+        {
+          ...config,
+        }
+      )
+      .then(this.handleResponse);
+  }
+
+  async post<B, R>(
+    url: string,
+    body: B,
+    config?: Omit<AxiosRequestConfig, "method" | "url">
+  ) {
+    return this.client
+      .post<B, AxiosResponse<R | RequestError>>(url, body, {
+        ...config,
+        headers: {
+          "Content-Type": "application/json",
+          ...(config?.headers ?? {}),
+        },
+      })
+      .then(this.handleResponse);
+  }
+
+  async put<B, R>(url: string, body: B) {
+    return this.client
+      .put<B, AxiosResponse<R | RequestError>>(url, body)
+      .then(this.handleResponse);
+  }
+
+  async patch<B, R>(url: string, body: B) {
+    return this.client
+      .patch<B, AxiosResponse<R | RequestError>>(url, body)
+      .then(this.handleResponse);
+  }
+
+  async delete<T>(url: string) {
+    return this.client
+      .delete<T | RequestError>(url)
+      .then(this.handleResponse);
+  }
+
+  handleResponse<B>(res: AxiosResponse<B | RequestError>) {
+    if (res.status >= 200 && res.status < 400) {
+      return res.data as B;
+    }
+    throw Error((res.data as RequestError)?.message);
+  }
+
+  cleanClient() {
+    this.client = axios.create({});
+  }
+
+  onError(error: unknown) {
+    console.error('[HTTP-CLIENT]:\n')
+    console.error(error)
+  }
+}
+
+export const http = new HttpClient();
