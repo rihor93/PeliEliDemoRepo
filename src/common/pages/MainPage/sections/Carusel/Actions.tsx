0cc8c21695d42814f837fdfdbfab644927e37321
lykasov_am <lykasov_am@lexema.ru>
2023-06-07 00:50:25 +0500

    DONE: actions carusel complete

---
+++ src/common/pages/MainPage/sections/Carusel/Actions.tsx
@@ -0,0 +1,14 @@
+import { Carousel } from "../../../../components";
+
+export const Actions: React.FC = () => {
+  return (
+    <section className='carusel page_carusel'>
+      <Carousel>
+        <img src="./gurmag_big.png"/>
+        <img src="./gurmag_big.png"/>
+        <img src="./gurmag_big.png"/>
+        <img src="./gurmag_big.png"/>
+      </Carousel>
+    </section>
+  )
+}
---
+++ src/common/pages/MainPage/sections/index.ts
@@ -0,0 +1,12 @@
+import { Actions } from './Carusel';
+import { Header } from './Header';
+import { Filter } from './Filter';
+import { Footer } from './Footer';
+import { Categories } from './Categories';
+export const Sections = {
+  carusel: Actions,
+  header: Header,
+  filter: Filter,
+  footer: Footer,
+  categories: Categories,
+}
