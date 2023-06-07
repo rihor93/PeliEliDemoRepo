2d3b0a6e20cb94a45dd8b343e1bf2c535f96d9f8
lykasov_am <lykasov_am@lexema.ru>
2023-06-07 20:24:38 +0500

    UPD: categories section

--- src/common/pages/MainPage/sections/Categories/Categories.tsx
+++ src/common/pages/MainPage/sections/Categories/Categories.tsx
@@ -1,5 +1,40 @@
-export const Categories: React.FC = () => {
-  return(
-    <section className='categories'>categories</section>
+import { useStore } from '../../../../hooks';
+import React from 'react';
+import './Categories.css';
+import { observer } from 'mobx-react-lite';
+
+
+export const Categories: React.FC = observer(() => {
+  const { mainPage } = useStore();
+  const { category_courses: categories } = mainPage;
+
+  return (
+    <section className='categories'>
+
+      {categories.map((category) => 
+        <div key={category.id} id={category.id}>
+          <h1>{category.category}</h1>
+          <div className="courses_list">
+
+            {category.courses.map((course, index) => 
+              <div 
+                className="course_item" 
+                key={`${category.id}-${index}`}
+              >
+                <img src={course.img} />
+                <h5>{course.title}</h5>
+                <p>{course.subtitle}</p>
+                <div>
+                  <img src="./cart.svg" />
+                  <span>{course.price}</span>
+                </div>
+              </div>
+            )}
+
+          </div>
+        </div>
+      )}
+
+    </section>
   )
-}
+})
