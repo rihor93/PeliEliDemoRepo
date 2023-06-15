0e2f19a4286829c38f43770a8a4deaf01a84e3ad
lykasov_am <lykasov_am@lexema.ru>
2023-06-15 19:36:57 +0500

    ADD: types

--- src/store/stores/types.d.ts
+++ src/store/stores/types.d.ts
@@ -14,3 +14,44 @@ type CategoryCourse = {
   MenuVCode: number,
   CourseList: CourseItem[],
 }
+
+/** Блюдо в корзине как часть заказа */
+type CouseInCart = {
+  couse: CourseItem;
+  quantity: number;
+  priceWithDiscount: number;
+  campaign?: number | undefined;
+}
+
+type PercentDiscount = {
+  vcode: number,
+  MinSum: number,
+  MaxSum: number,
+  bonusRate: number,
+  discountPercent: number,
+}
+
+type DishDiscount = {
+  vcode: number,
+  isset: number,
+  quantity: number,
+  promocode: string,
+  dish: number,
+  price: number,
+}
+
+type DishSetDiscount = {
+  vcode: number,
+  dishes: DishDiscount[],
+  dishCount: number,
+}
+
+type AllCampaignUser = {
+  Name: string,
+  Description: string,
+  VCode: number,
+  periodtype: string,
+  isset: number,
+  quantity: number,
+  promocode: string
+}
