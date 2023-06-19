6adfe443461212487c4683874c102d7afab93593
lykasov_am <lykasov_am@lexema.ru>
2023-06-19 17:00:04 +0500

    CHORE: small fixes

--- src/common/pages/MainPage/MainPage.tsx
+++ src/common/pages/MainPage/MainPage.tsx
@@ -10,8 +10,8 @@ export const MainPage: React.FC = observer(() => {
   const { selectedCourse, isLoading } = mainPage;
   return(
     <main className='page main_page'>
-      <button onClick={() => switchTheme('dark')}>to dark</button>
-      <button onClick={() => switchTheme('light')}>to light</button>
+      {/* <button onClick={() => switchTheme('dark')}>to dark</button>
+      <button onClick={() => switchTheme('light')}>to light</button> */}
       {selectedCourse && 
         <Modals.course course={selectedCourse} />
       }
