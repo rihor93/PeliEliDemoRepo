a4551513f579e1e58578d7284d9e06f56a8c0993
lykasov_am <lykasov_am@lexema.ru>
2023-06-21 20:17:13 +0500

    UPD: cart overlay icon

--- src/common/components/CartOverlay/CartOverlay.tsx
+++ src/common/components/CartOverlay/CartOverlay.tsx
@@ -1,14 +1,16 @@
 import { useNavigate } from 'react-router-dom';
 import './CartOverlay.css';
 
-export const CartOverlay: React.FC = () => {
+export const CartOverlay: React.FC<{
+  count: number
+}> = ({ count }) => {
   const navigate = useNavigate();
   return (
     <div className='cartOverlay' onClick={() => navigate('/cart')}>
       <img
         src="./cart.svg"
       />
-      <span>{0}</span>
+      <span>{count}</span>
     </div>
   )
 }
