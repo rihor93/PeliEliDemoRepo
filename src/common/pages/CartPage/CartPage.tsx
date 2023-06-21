429c9ea090ba1a57aa9dfdab4fab28a794ddff4d
lykasov_am <lykasov_am@lexema.ru>
2023-06-21 18:33:18 +0500

    UPD: страница корзины

--- src/common/pages/CartPage/CartPage.tsx
+++ src/common/pages/CartPage/CartPage.tsx
@@ -1,8 +1,55 @@
+import { observer } from 'mobx-react-lite';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useStore } from '../../hooks';
 import './CartPage.css';
 
-export const CartPage: React.FC = () => {
-  return(
-    <div>CartPage</div>
+export const CartPage: React.FC = observer(
+  () => {
+    const { cartStore } = useStore();
+    const navigate = useNavigate()
+
+    return (
+      <main className='cartPage'>
+        <section className='cartHeader'>
+          <button onClick={() => navigate(-1)}>{'<'}</button>
+          <h3>Корзина</h3>
+        </section>
+        <section className='cart page'>
+          {cartStore.isEmpty &&
+            <span>Корзина пуста</span>
+          }
+          {cartStore.items.map((item) =>
+            <CartItem courseInCart={item} />
+          )}
+        </section>
+        <section className='cartButton'>
+          <button disabled={cartStore.isEmpty}>
+            {`Оформить заказ за ${cartStore.totalPrice}`}
+          </button>
+        </section>
+      </main>
+    )
+  }
+)
+
+const CartItem: React.FC<{
+  courseInCart: CouseInCart
+}> = ({ courseInCart }) => {
+  return (
+    <div className='cartItem'>
+      <img src="./gurmag.png" />
+      <div className='cartItemBody'>
+        <div>
+          <span>{courseInCart.couse.Name}</span>
+          <p>{`ценв - ${courseInCart.priceWithDiscount} руб.`}</p>
+        </div>
+        <div>
+          <button className="minus">-</button>
+          <span className="count">{courseInCart.quantity}</span>
+          <button className="plus">+</button>
+        </div>
+      </div>
+    </div>
   )
 }
