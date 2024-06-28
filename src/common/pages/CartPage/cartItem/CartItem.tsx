abc88dbfe3522678f84a1e39bffb12fea0aafa3f
lykasov_am <lykasov_am@lexema.ru>
2024-06-28 13:24:05 +0500

    прелодеры в корзинке

--- src/common/pages/CartPage/cartItem/CartItem.tsx
+++ src/common/pages/CartPage/cartItem/CartItem.tsx
@@ -1,3 +1,4 @@
+import { Image, Skeleton } from "antd-mobile";
 import { observer } from "mobx-react-lite";
 import { GurmagLogo, LightMinus, DarkMinus, LightPlus, DarkPlus } from "../../../../assets";
 import { config } from "../../../configuration";
@@ -8,7 +9,7 @@ import { Optional, Undef } from "../../../types";
 const CartItem: React.FC<{
   courseInCart: CouseInCart,
   add: () => void,
-  remove: () => void, 
+  remove: () => void,
   campaignAllInfo: Undef<AllCampaignUser>,
   text: Optional<string>
 }> = observer(({ courseInCart, add, remove, ...rest }) => {
@@ -17,25 +18,49 @@ const CartItem: React.FC<{
   const isDarkMode = theme === 'dark';
 
   const { campaignAllInfo, text } = rest
-  
+
+
+  const ImagePreloader = () =>
+    <Skeleton animated style={{ width: '160px', height: '160px', }} />
+
+  const ImageFallback = () =>
+    <img src={GurmagLogo} style={{ objectFit: 'cover', width: '160px', height: '160px' }} />
+
   return (
     <div className='cartItem'>
-      <img
-        src={`${config.apiURL}/api/v2/image/Material?vcode=${courseInCart.couse.VCode}&compression=true&random=${session}`}
-        onError={replaceImgSrc(GurmagLogo)}
-      />
+      <div style={{ width: '160px', height: '160px' }}>
+        <Image
+          src={config.apiURL
+            + '/api/v2/image/Material?vcode='
+            + courseInCart.couse.VCode
+            + '&compression=true&random='
+            + session
+          }
+          placeholder={<ImagePreloader />}
+          fallback={<ImageFallback />}
+          alt={courseInCart.couse.Name}
+          fit='cover'
+          style={{
+            objectFit: 'cover',
+            "--width": '160px',
+            "--height": '160px',
+            display: 'flex'
+          }}
+        />
+      </div>
+
       <div className='cartItemBody'>
         <div>
           <span>{courseInCart.couse.Name}</span>
         </div>
-        {!campaignAllInfo 
+        {!campaignAllInfo
           ? null
           : <span style={{ color: 'var(--gurmag-accent-color)' }}>
             {`Акция - ${campaignAllInfo.Name.replace(/ *\{[^}]*\} */g, "")}`}
-            </span>
+          </span>
         }
         <span style={{ color: 'var(--gurmag-accent-color)' }}>{text}</span>
-        
+
         <div className="row">
           {courseInCart.priceWithDiscount >= courseInCart.couse.Price * courseInCart.quantity
             ? null
@@ -58,10 +83,10 @@ const CartItem: React.FC<{
           </div>
           <h5>{`${Math.ceil(courseInCart.priceWithDiscount * 10) / 10} ₽`}</h5>
         </div>
-        {courseInCart.couse.NoResidue 
+        {courseInCart.couse.NoResidue
           ? null
           : courseInCart.couse.EndingOcResidue < courseInCart.quantity
-            ? <div 
+            ? <div
               style={{
                 position: 'absolute',
                 right: '0px',
@@ -73,14 +98,14 @@ const CartItem: React.FC<{
                 fontSize: '14px',
                 fontWeight: '700',
                 color: 'white',
-                
+
               }}
             >
               <p>{`сегодня осталось ${courseInCart.couse.EndingOcResidue}`}</p>
             </div>
             : null
         }
-        
+
       </div>
     </div>
   )
